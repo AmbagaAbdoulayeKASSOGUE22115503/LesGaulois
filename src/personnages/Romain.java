dc7e2b6d785efd5773d398b2c260026a3a4dc96b
AmbagaAbdoulayeKASSOGUE22115503 <ambaga.kass@gmail.com>
2022-10-16 17:23:57 +0200

    Les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,16 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement=0;
+	
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
-		assert (force>0);  
+		equipements=new Equipement[3];
+		assert (force>0); 
+		
 		
 	}
 	
@@ -40,19 +45,53 @@ public class Romain {
 		assert (force1>=force):"Préconditionn";
 	}
 	
+	public void ajouterEquipement(Equipement equipement) {
+		nbEquipement+=1;
+		equipements[nbEquipement]=equipement;
+		System.out.println("Le soldat "+nom+" s’équipe avec un "+equipement);
+		
+	}
 	
 	public String toString() {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
 	}
 	
+	public void sEquiper(Equipement equipement) {
+		switch ( nbEquipement) {
+		case 2:
+			System.out.println("Le soldat "+nom+" est déjà bien protégé");
+			break;
+		case 1:
+			if ( equipements[1]==equipement) {
+				System.out.println("Le soldat "+nom+" possède déjà un "+equipement);
+			}
+			else {
+				ajouterEquipement(equipement);
+			}
+			break;
+		default:
+			nbEquipement+=1;
+			equipements[nbEquipement]=equipement;
+			System.out.println("Le soldat "+nom+" s’équipe avec un "+equipement);
+			break;
+		}
+
+		}
+		
 	
 	public static void main(String[] args) {
 		
 		Romain minus= new Romain(" Minus",6);
-		System.out.println(minus);
-		minus.parler("Je suis minus et je suis un Romain!");
-		minus.recevoirCoup(3);
+//		System.out.println(minus);
+//		minus.parler("Je suis minus et je suis un Romain!");
+//		minus.recevoirCoup(3);
+//		System.out.println(Equipement.BOUCLIER);
+//		System.out.println(Equipement.CASQUE);
 		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 	
 	
