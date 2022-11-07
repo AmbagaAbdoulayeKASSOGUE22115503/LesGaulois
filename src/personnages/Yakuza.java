a6b6d9ba88d2d0c4e9c17f867224fb5eb0ab941e
AmbagaAbdoulayeKASSOGUE22115503 <ambaga.kass@gmail.com>
2022-11-07 19:21:36 +0100

    création de la classe Yakuza

---
+++ src/personnages/Yakuza.java
@@ -0,0 +1,35 @@
+package personnages;
+
+public class Yakuza extends Humain{
+	private String clan;
+	private int reputation=0;
+	
+	public Yakuza(String nom, String boissonPreferee, int argent, String clan) {
+		super(nom,boissonPreferee,argent);
+		this.clan= clan;
+	}
+	
+	
+	public String getClan() {
+		return clan;
+	}
+	
+	public int getReputation() {
+		return reputation;
+	}
+	
+	public void extorquer(Commercant victime) {
+		super.parler(" Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ? ");
+		victime.parler(victime.getNom()+" , si tu tiens à la vie donne moi ta bourse !");
+		
+		int argentExtorquer= victime.getArgent();
+		super.setArgent(argentExtorquer+super.getArgent());
+		
+		victime.seFaireExtorquer();
+		super.parler(" J’ai piqué les "+argentExtorquer+" sous de "+victime.getNom()+", ce qui \r\n"
+				+ "me fait "+super.getArgent()+" sous dans ma poche. Hi ! Hi !");
+		reputation++;
+		
+	}
+	
+}
