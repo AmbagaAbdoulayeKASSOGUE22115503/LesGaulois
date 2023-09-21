349d6e1184a540ec0b2c3881e237739c6a5c0d57
AmbagaAbdoulayeKASSOGUE22115503 <ambaga.kass@gmail.com>
2023-09-21 11:59:18 +0200

    TP1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,44 @@
+package personnages;
+
+public class Gaulois {
+	
+	private String nom;
+	private int force;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<< " + texte + " >>");
+		
+		
+	}
+
+	private String prendreParole() {
+		return "Le gaulois "+ nom + " : ";
+	}
+	
+	public void fraper(Romain romain) {
+		System.out.println(nom+ " envoie un grand coup dans la mâchoire de  "+ romain.getNom() );
+		romain.recevoirCoup(force / 3);
+	}
+	
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + "]";
+	}
+
+	public static void main(String[] args) {
+		Gaulois asterix= new Gaulois("Asterix", 8);
+		System.out.println(asterix.getNom());
+		System.out.println(asterix);
+
+	}
+	
+}
